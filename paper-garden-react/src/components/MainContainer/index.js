4894d6b215c1a1855763910317fd96e45ea53a5d
William Weisberger <bill.weisberger@gmail.com>
2019-09-04 13:17:09 -0600

    first commit

---
+++ paper-garden-react/src/components/MainContainer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class MainContainer extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                Hi
+            </div>
+        )
+    }
+}
