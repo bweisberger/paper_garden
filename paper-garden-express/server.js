d2e85951926df7b3d0b67d6b42c4872972777341
William Weisberger <bill.weisberger@gmail.com>
2019-09-06 11:11:13 -0600

    added express side of project

---
+++ paper-garden-express/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', async (req, res) =>{
+    try{
+        
+    }
+})
+
+app.listen(3333, ()=>{
+    console.log('express server listening on port 3333')
+})
